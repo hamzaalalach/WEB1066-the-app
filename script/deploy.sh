fab27eaf2a7c06ca88404dd7facb394f11089dfb
Hamza Alalach <hamzaalalach@gmail.com>
2020-08-05 16:35:27 +0100

    Fix typos

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 
 docker login -u="$DOCKER_LOGIN" -p="$DOCKER_PASSWORD"
-docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
-docker push $DOCKER_LOGIN/monolithic-shop:latest
+docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066-theapp:latest
+docker push $DOCKER_LOGIN/web1066-theapp:latest
