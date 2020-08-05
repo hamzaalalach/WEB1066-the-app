5be229e7e412750144389d22efac2ab31e422289
Hamza Alalach <hamzaalalach@gmail.com>
2020-08-05 21:48:35 +0100

    Debug 1

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+./gradlew -p ./monolithic/ui packageToContainer
 docker login -u="$DOCKER_LOGIN" -p="$DOCKER_PASSWORD"
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066-theapp:latest
 docker push $DOCKER_LOGIN/web1066-theapp:latest
